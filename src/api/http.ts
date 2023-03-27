ef2ed7a70e8d97a8dff7730a578c9bf9d7436a60
rafaelreisramos <rrramos@gmail.com>
2023-03-27 17:43:46 -0300

    chore: use env variables for base_url

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-const http = axios.create({
-  baseURL: 'http://localhost:3333',
-})
+const http = axios.create({ baseURL: import.meta.env.VITE_API_URL })
 
 export { http }
